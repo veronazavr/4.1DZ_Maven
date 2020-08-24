d330a0c73540bc3a0d4d3ab30fb405765d782450
Veronazavr <verochka10@list.ru>
2020-08-24 23:34:41 +0300

    Changed

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -37,4 +37,36 @@ class BonusServiceTest {
         assertEquals(expected, actual);
 
     }
+    @Test
+    void shoeldCalculateForNotAllRegisteredUnderLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 100_600_0;
+        boolean registered = false;
+        long expected = 100;
+
+        //вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
+    @Test
+    void shoeldCalculateForAllRegisteredUnderLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1000_600;
+        boolean registered = true;
+        long expected = 300;
+
+        //вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+
+    }
+
 }
